8d661f3e809f8f92328a483464a8adba773cd1eb
Victor Loginov <tkoe88@zoho.com>
2023-08-16 15:06:32 +0200

    progress

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,10 +1,25 @@
 class RestaurantsController < ApplicationController
   def index
-  end
-
-  def new
+    @restaurants = Restaurant.all
   end
 
   def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.save
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :category, :phone_number, :rating)
   end
 end
