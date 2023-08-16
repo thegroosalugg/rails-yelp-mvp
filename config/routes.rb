0b4e3c519e1fd36097b33ada74d1c9dfaa1a68c4
Victor Loginov <tkoe88@zoho.com>
2023-08-16 12:21:55 +0200

    created controllers and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'reviews/index'
+  get 'reviews/new'
+  get 'reviews/_review_form'
+  get 'restaurants/index'
+  get 'restaurants/new'
+  get 'restaurants/show'
+  get 'restaurants/_restaurant_form'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
