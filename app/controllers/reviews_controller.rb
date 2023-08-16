d71ca8cc4ce3124fe243f8728c8e1e4284ba1c3f
Victor Loginov <tkoe88@zoho.com>
2023-08-16 16:28:59 +0200

    nearly there

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,18 @@
 class ReviewsController < ApplicationController
-  def new
+  def create
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new(review_params)
+    @review.restaurant = @restaurant
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:restaurant, :content, :rating)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 Rails.application.routes.draw do
-  get 'reviews/index'
-  get 'reviews/new'
-  get 'reviews/_review_form'
-  get 'restaurants/index'
-  get 'restaurants/new'
-  get 'restaurants/show'
-  get 'restaurants/_restaurant_form'
+  # get 'reviews/index'
+  # get 'reviews/new'
+  # get 'reviews/_review_form'
+  # get 'restaurants/index'
+  # get 'restaurants/new'
+  # get 'restaurants/show'
+  # get 'restaurants/_restaurant_form'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :restaurants
+  resources :restaurants do
+    resources :reviews
+  end
 end
