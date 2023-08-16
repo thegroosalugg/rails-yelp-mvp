d54f88345081a1a23f6cd4345930a2fac014d3da
Victor Loginov <tkoe88@zoho.com>
2023-08-16 12:00:25 +0200

    seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+dishoom = {name: "Dishoom", address: "7 Boundary St, London E2 7JE", category: "japanese"}
+pizza_east = {name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", category: "japanese"}
+the_big_easy = {name: "The Big Easy", address: "123 Fake Street, Berlin 10553", category: "japanese"}
+jons_diner = {name: "Jon's Diner", address: "46 On the Moon, The Moon 5871", category: "japanese"}
+planet_eat = {name: "Planet Eat", address: "1 In the Desert, The Sea 11111", category: "japanese"}
+
+[dishoom, pizza_east, the_big_easy, jons_diner, planet_eat].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
